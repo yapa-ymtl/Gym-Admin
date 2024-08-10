a489cd734dc040c078e1baec8137db84ab084eac
Tharindu Lakmal Yapa <51540855+yapa-ymtl@users.noreply.github.com>
2024-08-10 14:45:41 +0530

    Delete build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GymAdmin_FILE C:/myWork/GymAdmin/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/GymAdmin.exe)
-set(__QT_DEPLOY_TARGET_GymAdmin_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GymAdmin_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
